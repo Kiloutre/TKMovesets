f57dc43b8d950f766713d9ae120ca0b043f6fce9
Kiloutre <kilowwwww@gmail.com>
2023-03-03 00:14:49 +0100

    Finish writing CanImport() for T7

--- TekkenTools/GameInteractions/Importers/Importer_t7.cpp
+++ TekkenTools/GameInteractions/Importers/Importer_t7.cpp
@@ -22,6 +22,24 @@ void ImporterT7::Import(const char* filename, gameAddr playerAddress, float* pro
 
 bool ImporterT7::CanImport()
 {
-	// todo
-	return true;
+	gameAddr playerAddress = m_game->ReadPtr("p1_addr");
+	// We'll just read through a bunch of values that wouldn't be valid if a moveset wasn't loaded
+	// readInt64() may return -1 if the read fails so we have to check for this value as well.
+
+	if (playerAddress == 0 || playerAddress == -1) {
+		return false;
+	}
+
+	gameAddr currentMove = m_process->readInt64(playerAddress + 0x220);
+	if (currentMove == 0 || currentMove == -1) {
+		return false;
+	}
+
+	gameAddr animAddr = m_process->readInt64(currentMove + 0x10);
+	if (animAddr == 0 || animAddr == -1) {
+		return false;
+	}
+
+	uint8_t animType = m_process->readInt8(animAddr);
+	return animType == 0x64 || animType == 0xC8;
 }
