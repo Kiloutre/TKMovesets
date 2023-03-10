802eded41de0562197ba9e9a0d1e8531dc8854ef
Kiloutre <kilowwwww@gmail.com>
2023-03-10 06:24:49 +0100

    Debug test

---
+++ TKMovesets2/main_test.cpp
@@ -0,0 +1,120 @@
+﻿// CMakeProject1.cpp : définit le point d'entrée de l'application.
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "ImGui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <stdio.h>
+
+using namespace std;
+
+// -- Static helpers -- //
+
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+// -- main -- //
+
+int main(int argc, wchar_t** argv, char** env)
+{
+
+	// Initialize GLFW library
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit()) {
+		return 1;
+	}
+
+	// GL 3.0 + GLSL 130
+	// Set the window hint. Not really that important to be honest.
+	const char* c_glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "test program", nullptr, nullptr);
+
+	if (window == nullptr) {
+		return 2;
+	}
+
+	// Set window for current thread
+	glfwMakeContextCurrent(window);
+	// Enable vsync
+	glfwSwapInterval(1);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		throw("Unable to context to OpenGL");
+	}
+
+	{
+		// Set viewport for OpenGL
+		int screen_width, screen_height;
+		glfwGetFramebufferSize(window, &screen_width, &screen_height);
+		glViewport(0, 0, screen_width, screen_height);
+	}
+
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.IniFilename = nullptr;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(c_glsl_version);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		
+		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		////
+
+		ImGui::Begin("window1");
+		if (ImGui::Button("Click me"))
+		{
+			ImGui::Text("test");
+		}
+		ImGui::End();
+
+		ImGui::Begin("window2");
+		if (ImGui::Button("Click me"))
+		{
+			ImGui::Text("test");
+		}
+		ImGui::End();
+
+
+		////
+
+		ImGui::Render();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
