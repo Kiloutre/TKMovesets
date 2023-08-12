4433438075c8fd186870dda6decbde894fee1671
kilo <kilowwwww@gmail.com>
2023-08-12 10:42:53 +0200

    Add round reset

--- TKMovesets2/Injected/MovesetLoaders/MovesetLoader_t8.cpp
+++ TKMovesets2/Injected/MovesetLoaders/MovesetLoader_t8.cpp
@@ -14,6 +14,7 @@ using namespace StructsT8;
 // Reference to the MovesetLoader
 MovesetLoaderT8* g_loader = nullptr;
 std::ofstream* g_log = nullptr;
+unsigned int g_roundType = 0;
 
 # define GET_HOOK(x) ((uint64_t)&T8Hooks::x)
 
@@ -22,6 +23,8 @@ std::ofstream* g_log = nullptr;
 namespace T8Functions
 {
 	typedef void** (*Log)(void** a1, const char* a2, size_t a3, int64 a4);
+
+	typedef char (*ResetRound)(unsigned int a1);
 }
 
 // -- Helpers --
@@ -49,7 +52,6 @@ namespace T8Hooks
 		}
 		return g_loader->CastTrampoline<T8Functions::Log>("TK__Log")(a1, format, string_length, a4);
 	}
-
 }
 
 // -- -- //
@@ -65,18 +67,19 @@ void MovesetLoaderT8::InitHooks()
 	m_requiredSymbols = {
 		// Hooks
 		// Functions
+		"TK__ResetRound"
 	};
 
 	// Crucial functions / hooks
 
 	// Other less important things
 
-	{
 #ifdef BUILD_TYPE_DEBUG
-		// Find TK__Log
-		RegisterHook("TK__Log", m_moduleName, "f_Log", GET_HOOK(Log));
-#endif
-	}
+	// Find TK__Log
+	RegisterHook("TK__Log", m_moduleName, "f_Log", GET_HOOK(Log));
+#endif sk
+	RegisterFunction("TK__ResetRound", m_moduleName, "f_ResetRound");
+
 }
 void MovesetLoaderT8::OnInitEnd()
 {
@@ -87,8 +90,13 @@ void MovesetLoaderT8::OnInitEnd()
 	// Apply the hooks that need to be applied immediately
 
 	// Other
+#ifdef BUILD_TYPE_DEBUG
 	HookFunction("TK__Log");
+#endif
 	
+	DEBUG_LOG("g_roundType = %p\n", &g_roundType);
+
+
 	std::string filename = std::format("G:/Logs_{}.txt", Helpers::formatDateTime(Helpers::getCurrentTimestamp(), true));
 	g_log = new std::ofstream(filename);
 }
@@ -113,6 +121,20 @@ void MovesetLoaderT8::Mainloop()
 void MovesetLoaderT8::Debug()
 {
 #ifdef BUILD_TYPE_DEBUG
-
+	void* func_addr;
+	switch (g_roundType)
+	{
+	case 0:
+		func_addr = (void*)0x14153DE80;
+		break;
+	case 1:
+		func_addr = (void*)0x14153DF20;
+		break;
+	default:
+		return;
+	}
+	void (*test)(void) = (void(*)(void))func_addr;
+	test();
+	//g_loader->CastFunction<T8Functions::ResetRound>("TK__ResetRound")(g_roundType);
 #endif
 }
